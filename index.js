07854ef1a1cd7f663866159d9276587dbd8ba455
senakamura <seanenakamura@gmail.com>
2016-03-23 23:29:09 -1000

    parses a close file values into css for red and blue

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 'use strict'
 var fs = require('fs');
-
-'use strict'
-var fs = require('fs');
+var CSSJSON = require('cssjson');
 
 var source = process.argv.splice(2)[0];
 var target = source.substring(0, source.lastIndexOf('.')) + '.css';
 
-fs.readFile(source, 'utf-8', function(err, data){ 
-  if err throw err;
+fs.readFile(source, 'utf-8', function(err, data){
+  if (err){throw err;}
 
-  fs.writeFile(target, data, function(err, data) {
-    if err throw err;
-
-    console.log('Wrote ' + target + '!'); 
+  var parser = function (input){
+    var css = CSSJSON.toJSON(input);
+    for (var key in css.children){
+      var val = css.children[key].attributes;
+      switch (val.somewhat){
+        case '"blueish"':
+          val.somewhat = '"blue"';
+          break;
+        case '"redish"':
+          val.somewhat = '"red"';
+          break;
+        default:
+      }
+    }
+    return CSSJSON.toCSS(css);
   };
-});
-var source = process.argv.splice(2)[0];
-var target = source.substring(0, source.lastIndexOf('.')) + '.css';
 
-fs.readFile(source, 'utf-8', function(err, data){ 
-  if err throw err;
+  var output = parser(data);
 
-  fs.writeFile(target, data, function(err, data) {
-    if err throw err;
-
-    console.log('Wrote ' + target + '!'); 
-  };
+  fs.writeFile(target, output, function(err) {
+      if (err) {throw err;}
+      console.log('Wrote ' + target + '!');
+   });
 });
