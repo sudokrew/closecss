40ff2b1c387d1fb79a95988e6bed928399b955b9
senakamura <seanenakamura@gmail.com>
2016-03-24 21:38:58 -1000

    converts to postcss plugin style

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
 'use strict'
-var fs = require('fs');
-var CSSJSON = require('cssjson');
+var fs = require('fs');dd
+var postcss = require('postcss');
 
 var source = process.argv.splice(2)[0];
 var target = source.substring(0, source.lastIndexOf('.')) + '.css';
 
-fs.readFile(source, 'utf-8', function(err, data){
-  if (err){throw err;}
+module.exports = postcss.plugin('closecss', function (opts){
+  opts = opts || {};
 
-  var parser = function (input){
-    var css = CSSJSON.toJSON(input);
-    for (var key in css.children){
-      var val = css.children[key].attributes;
-      switch (val.somewhat){
-        case '"blueish"':
-          val.somewhat = '"blue"';
-          break;
-        case '"redish"':
-          val.somewhat = '"red"';
-          break;
-        default:
-      }
-    }
-    return CSSJSON.toCSS(css);
+  return function (css){
+    css.eachDecl(function transformDecl(decl){
+      console.log(decl.prop);
+      console.log(decl.value);
+    });
   };
-
-  var output = parser(data);
-
-  fs.writeFile(target, output, function(err) {
-      if (err) {throw err;}
-      console.log('Wrote ' + target + '!');
-   });
 });
