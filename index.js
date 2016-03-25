0ce66c140c62f68e4efe96c7945ee5b8d2f2d9d1
Jay Won <jason@sudokrew.com>
2016-03-25 00:31:57 -1000

    Prototype of working plugin and use case

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-'use strict'
-var fs = require('fs');
-var CSSJSON = require('cssjson');
+'use strict';
+var postcss   = require('postcss');
+var fs        = require('fs');
+var closecss  = require('closecss');
+var source = 'src/styles.close';
+var target = 'dest/styles.css';
 
-var source = process.argv.splice(2)[0];
-var target = source.substring(0, source.lastIndexOf('.')) + '.css';
-
-fs.readFile(source, 'utf-8', function(err, data){
-  if (err){throw err;}
-
-  var parser = function (input){
-    var css = CSSJSON.toJSON(input);
-    for (var key in css.children){
-      var val = css.children[key].attributes;
-      switch (val.somewhat){
-        case '"blueish"':
-          val.somewhat = '"blue"';
-          break;
-        case '"redish"':
-          val.somewhat = '"red"';
-          break;
-        default:
-      }
-    }
-    return CSSJSON.toCSS(css);
-  };
-
-  var output = parser(data);
-
-  fs.writeFile(target, output, function(err) {
-      if (err) {throw err;}
-      console.log('Wrote ' + target + '!');
-   });
+var processor = postcss([closecss]);
+var css = fs.readFile(source, 'utf-8', function (err, data){
+  if(err) return console.log(err);
+  processor
+    .process(data, {from: source, to: target})
+    .then(function(result) {
+      fs.writeFile(target, result);
+    });
 });
+
