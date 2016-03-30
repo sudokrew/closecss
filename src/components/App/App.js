d45734be6df08678026fb7bd69b9a943e1e083ed
Emmanuel Pilande <epilande@gmail.com>
2016-03-30 04:37:38 -1000

    Convert svg logo to react component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './App.scss';
+import Logo from 'components/Logo';
 
 const App = () => (
   <div className="container">
     <h1 className={styles.heading}>CLOSE CSS</h1>
+    <Logo />
     <section className={styles.section}>
       <div>1</div>
       <div>2</div>
