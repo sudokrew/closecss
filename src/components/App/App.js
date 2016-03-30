fee541ed387a85be371408e12882e79bf58e1c1d
Emmanuel Pilande <epilande@gmail.com>
2016-03-30 03:19:21 -1000

    Import variables instead of colors

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,11 @@ import styles from './App.scss';
 const App = () => (
   <div className="container">
     <h1 className={styles.heading}>CLOSE CSS</h1>
+    <section className={styles.section}>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+    </section>
     <button>Button</button>
   </div>
 );
