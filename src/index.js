64ee60f135929290a101ed8265294a0f7e1e10dd
Emmanuel Pilande <epilande@gmail.com>
2016-03-31 00:33:28 -1000

    Move vendor css into index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from 'components/App';
 
+import 'sanitize.css/sanitize.css';
+import 'highlight.js/styles/tomorrow-night.css';
+
 import 'styles/app.scss';
 
 render(
