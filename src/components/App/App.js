d2c2d5151423f12b92fad6c5aa048c03904d6c36
Emmanuel Pilande <epilande@gmail.com>
2016-03-31 01:36:53 -1000

    Add features section

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -36,45 +36,81 @@ const App = () => (
       </div>
     </div>
     <div className={styles.how}>
-      <h2>How It Works</h2>
       <div className={styles.container}>
-        <div className={styles.howColumn}>
-          <h5>You put close enough CSS in</h5>
-          <Highlight className="css">
-            {"body {"} <br/>
-            {"  height: 100vh;"} <br/>
-            {"  background: somewhat('blueish');"} <br/>
-            {"  color: somewhat('orangeish');"} <br/>
-            {"  font-family: Helvetica, sans-serif;"} <br/>
-            {"}"} <br/>
-            <br/>
-            {".container {"} <br/>
-            {"  width: somewhat('largish');"} <br/>
-            {"  margin: somewhat('tallish') auto;"} <br/>
-            {"}"}
-          </Highlight>
-        </div>
-        <div className={styles.howColumn}>
-          <h5>And get close enough CSS out</h5>
-          <Highlight className="css">
-            {"body {"} <br/>
-            {"  height: 100vh;"} <br/>
-            {"  background: #0000FF;"} <br/>
-            {"  color: #F3A618;"} <br/>
-            {"  font-family: Helvetica, sans-serif;"} <br/>
-            {"}"} <br/>
-            <br/>
-            {".container {"} <br/>
-            {"  width: 1024px;"} <br/>
-            {"  margin: 100px auto;"} <br/>
-            {"}"}
-          </Highlight>
+        <h2>How It Works</h2>
+        <div className={styles.row}>
+          <div className={styles.twoColumn}>
+            <h5>You put close enough CSS in</h5>
+            <Highlight className="css">
+              {"body {"} <br/>
+              {"  height: 100vh;"} <br/>
+              {"  background: somewhat('blueish');"} <br/>
+              {"  color: somewhat('orangeish');"} <br/>
+              {"  font-family: Helvetica, sans-serif;"} <br/>
+              {"}"} <br/>
+              <br/>
+              {".container {"} <br/>
+              {"  width: somewhat('largish');"} <br/>
+              {"  margin: somewhat('tallish') auto;"} <br/>
+              {"}"}
+            </Highlight>
+          </div>
+          <div className={styles.twoColumn}>
+            <h5>And get close enough CSS out</h5>
+            <Highlight className="css">
+              {"body {"} <br/>
+              {"  height: 100vh;"} <br/>
+              {"  background: #0000FF;"} <br/>
+              {"  color: #F3A618;"} <br/>
+              {"  font-family: Helvetica, sans-serif;"} <br/>
+              {"}"} <br/>
+              <br/>
+              {".container {"} <br/>
+              {"  width: 1024px;"} <br/>
+              {"  margin: 100px auto;"} <br/>
+              {"}"}
+            </Highlight>
+          </div>
         </div>
       </div>
     </div>
     <div className={styles.taglineBase}>
       <div className={styles.container}>
         <h2 className={styles.tagline}>A CSS preprocessor for when you've... almost got it.</h2>
+      </div>
+    </div>
+    <div className={styles.features}>
+      <div className={styles.container}>
+        <div className={styles.twoColumn}>
+          <h2>What is CloseCSS</h2>
+          <p>Consectetur inventore corporis nobis neque molestiae! Libero tempore ducimus
+            deleniti tenetur voluptatibus. Quos quis nostrum delectus nulla omnis tempore.</p>
+        </div>
+        <div className={styles.twoColumn}>
+          <h2>Features</h2>
+          <div className={styles.row}>
+            <div className={styles.twoColumn}>
+              <ul>
+                <li>tallish</li>
+                <li>whatever</li>
+                <li>blueish</li>
+                <li>largeish</li>
+                <li>redish</li>
+                <li>curvish</li>
+              </ul>
+            </div>
+            <div className={styles.twoColumn}>
+              <ul>
+                <li>smallish</li>
+                <li>thickish</li>
+                <li>roundish</li>
+                <li>boldish</li>
+                <li>farish</li>
+                <li>mediumish</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
