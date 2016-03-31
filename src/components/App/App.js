3fbf0dfee30a753f39b511b7316c4d3940e82dfb
Emmanuel Pilande <epilande@gmail.com>
2016-03-30 23:08:33 -1000

    Add tagline section

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,20 +8,27 @@ const App = () => (
   <div className={styles.base}>
     <Nav />
     <div className={styles.hero}>
-      <div className={styles.logo}>
-        <Logo />
-      </div>
-      <div className={styles.heroText}>
-        <h1>Fuck it, close enough.</h1>
-        <p>
-          Lorem aperiam natus soluta porro hic accusamus odit.
-          Cum voluptatem quia eaque beatae sunt?
-          Nostrum saepe deleniti quia facere mollitia.
-        </p>
-        <div className={styles.action}>
-          <Button href="#" type="secondary">Learn More</Button>
-          <Button href="#" type="primary">View on GitHub</Button>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <Logo />
         </div>
+        <div className={styles.heroText}>
+          <h1>Fuck it, close enough.</h1>
+          <p>
+            Lorem aperiam natus soluta porro hic accusamus odit.
+            Cum voluptatem quia eaque beatae sunt?
+            Nostrum saepe deleniti quia facere mollitia.
+          </p>
+          <div className={styles.action}>
+            <Button href="#" type="secondary">Learn More</Button>
+            <Button href="#" type="primary">View on GitHub</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className={styles.install}>
+      <div className={styles.container}>
+        <h2 className={styles.tagline}>A CSS preprocessor for when you've... almost got it.</h2>
       </div>
     </div>
   </div>
