23bd0b9941c34db606b3dadd827c5ecd7559e25b
Jay Won <jason@sudokrew.com>
2016-03-31 02:06:42 -1000

    Refactor demo to make no reference to postcss outside of closecss module.

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 'use strict';
-var postcss   = require('postcss');
 var fs        = require('fs');
 var closecss  = require('closecss');
 
 var source = 'src/styles.close';
 var target = 'dest/styles.css';
 
-var processor = postcss([closecss]);
 var css = fs.readFile(source, 'utf-8', function (err, data){
   if(err) return console.log(err);
-  processor
+  closecss
     .process(data, {from: source, to: target})
     .then(function(result) {
       fs.writeFile(target, result);
