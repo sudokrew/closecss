e1f02e22506805c9193b378eba1b6bd044cba878
Emmanuel Pilande <epilande@gmail.com>
2016-03-30 19:08:34 -1000

    Style hero text

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,7 +10,16 @@ const App = () => (
       <div className={styles.logo}>
         <Logo />
       </div>
-      <h1>A CSS preprocessor for when you've... almost got it.</h1>
+      <div className={styles.heroText}>
+        <h1>Fuck it, close enough.</h1>
+        <p>
+          Lorem aperiam natus soluta porro hic accusamus odit.
+          Cum voluptatem quia eaque beatae sunt?
+          Nostrum saepe deleniti quia facere mollitia.
+        </p>
+        <a href="#">Learn More</a>
+        <a href="#">GitHub</a>
+      </div>
     </div>
   </div>
 );
