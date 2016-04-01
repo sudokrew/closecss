165cc486d359e97c932450af9d1815585806c193
Emmanuel Pilande <epilande@gmail.com>
2016-03-31 20:15:17 -1000

    Use Container, Row, & Col components in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Highlight from 'react-highlight';
+import Container from 'components/grid/Container';
+import Row from 'components/grid/Row';
+import Col from 'components/grid/Col';
 import Nav from 'components/Nav';
-import Logo from 'components/Logo';
-import Button from 'components/Button';
+import Hero from 'components/Hero';
 import styles from './App.scss';
 
 const App = () => (
@@ -10,18 +12,18 @@ const App = () => (
     <Nav />
     <Hero />
     <div className={styles.install}>
-      <div className={styles.container}>
+      <Container>
         <h2>Installation</h2>
         <Highlight className="console">
           {"$ npm install closecss"}
         </Highlight>
-      </div>
+      </Container>
     </div>
     <div className={styles.how}>
-      <div className={styles.container}>
+      <Container>
         <h2>How It Works</h2>
-        <div className={styles.row}>
-          <div className={styles.twoColumn}>
+        <Row>
+          <Col size={6}>
             <h5>You put close enough CSS in</h5>
             <Highlight className="css">
               {"body {"} <br/>
@@ -36,8 +38,8 @@ const App = () => (
               {"  margin: somewhat('tallish') auto;"} <br/>
               {"}"}
             </Highlight>
-          </div>
-          <div className={styles.twoColumn}>
+          </Col>
+          <Col size={6}>
             <h5>And get close enough CSS out</h5>
             <Highlight className="css">
               {"body {"} <br/>
@@ -52,48 +54,50 @@ const App = () => (
               {"  margin: 100px auto;"} <br/>
               {"}"}
             </Highlight>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
     <div className={styles.taglineBase}>
-      <div className={styles.container}>
+      <Container>
         <h2 className={styles.tagline}>A CSS preprocessor for when you've... almost got it.</h2>
-      </div>
+      </Container>
     </div>
     <div className={styles.features}>
-      <div className={styles.container}>
-        <div className={styles.twoColumn}>
-          <h2>What is CloseCSS</h2>
-          <p>Consectetur inventore corporis nobis neque molestiae! Libero tempore ducimus
-            deleniti tenetur voluptatibus. Quos quis nostrum delectus nulla omnis tempore.</p>
-        </div>
-        <div className={styles.twoColumn}>
-          <h2>Features</h2>
-          <div className={styles.row}>
-            <div className={styles.twoColumn}>
-              <ul>
-                <li>tallish</li>
-                <li>whatever</li>
-                <li>blueish</li>
-                <li>largeish</li>
-                <li>redish</li>
-                <li>curvish</li>
-              </ul>
+      <Container>
+        <Row>
+          <Col size={6}>
+            <h2>What is CloseCSS</h2>
+            <p>Consectetur inventore corporis nobis neque molestiae! Libero tempore ducimus
+              deleniti tenetur voluptatibus. Quos quis nostrum delectus nulla omnis tempore.</p>
+          </Col>
+          <Col size={6}>
+            <h2>Features</h2>
+            <div className={styles.row}>
+              <div className={styles.twoColumn}>
+                <ul>
+                  <li>tallish</li>
+                  <li>whatever</li>
+                  <li>blueish</li>
+                  <li>largeish</li>
+                  <li>redish</li>
+                  <li>curvish</li>
+                </ul>
+              </div>
+              <div className={styles.twoColumn}>
+                <ul>
+                  <li>smallish</li>
+                  <li>thickish</li>
+                  <li>roundish</li>
+                  <li>boldish</li>
+                  <li>farish</li>
+                  <li>mediumish</li>
+                </ul>
+              </div>
             </div>
-            <div className={styles.twoColumn}>
-              <ul>
-                <li>smallish</li>
-                <li>thickish</li>
-                <li>roundish</li>
-                <li>boldish</li>
-                <li>farish</li>
-                <li>mediumish</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
     <div className={styles.footer}>
       <div className={styles.container}>
