54acd5a236e90e2e35c87325dc3c8e46df560d04
Emmanuel Pilande <epilande@gmail.com>
2016-04-01 12:52:26 -1000

    Animate meme

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Container from 'components/grid/Container';
 import Logo from 'components/Logo';
+import Meme from 'components/Meme';
 import Button from 'components/Button';
 import styles from './Hero.scss';
 
 const Hero = () => (
   <div className={styles.hero}>
-    <div className={styles.container}>
+    <Container>
       <div className={styles.logo}>
         <Logo fill="#FFFFFF" />
       </div>
@@ -20,7 +22,8 @@ const Hero = () => (
           <Button href="https://github.com/sudokrew/closecss" type="primary" target="_blank">View on GitHub</Button>
         </div>
       </div>
-    </div>
+    </Container>
+    <Meme className={styles.meme} fill="rgba(0, 0, 0, 0.05)" size={600} />
   </div>
 );
 
